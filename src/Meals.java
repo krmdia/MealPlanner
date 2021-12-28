f511dc15fcb396d0258373b10932f80a23cd5445
Karim Diallo <22224284+krmdia@users.noreply.github.com>
2021-12-28 04:21:45 -0500

    added GUI

--- src/Meals.java
+++ src/Meals.java
@@ -1,3 +1,6 @@
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -8,17 +11,77 @@ public class Meals {
 
     private List<String> meals;
     private List<String> mealPlan;
+    private String[] weekdays = {"Monday ", "Tuesday ", "Wednesday ", "Thursday ", "Friday ", "Saturday ", "Sunday "};
+    private JButton getMealsButton;
+    private JButton resetButton;
+    private JButton addMealButton;
+    private JButton getMealPlanButton;
+    private JTextArea Text;
+    private javax.swing.JPanel JPanel;
+    private JScrollPane ScrollPane;
+    private JButton ClearTextButton;
+    private JButton RemoveMealButton;
 
 
     public Meals(){
         this.meals = new ArrayList<>();
         this.mealPlan = new ArrayList<>();
+        getMealPlanButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+                if (meals.size()<6){
+                    Text.append("Add more meals before creating a meal plan!\n");
+                } else {
+
+                    createMealPlan();
+
+                    for (int i = 0; i < 7; i++) {
+                        Text.append(weekdays[i] + mealPlan.get(i) + "\n");
+                    }
+
+                }
+
+            }
+        });
+        addMealButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                meals.addAll(Arrays.asList(Text.getText().split("\\r?\\n")));
+            }
+        });
+        getMealsButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                for (String meal:
+                        meals) {
+                    if (meal.equals(meals.get(meals.size()-1))){
+                        Text.append(meal);
+                    } else Text.append(meal + "\n");
+                }
+            }
+        });
+        ClearTextButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                Text.setText(null);
+            }
+        });
+        RemoveMealButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                meals.removeAll(Arrays.asList(Text.getText().split("\\r?\\n")));
+            }
+        });
+        resetButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                meals.clear();
+                mealPlan.clear();
+            }
+        });
     }
 
-    public Meals(ArrayList<String> meals){
-        this.meals = meals;
-        this.mealPlan = new ArrayList<>();
-    }
 
     public void addMeal(String meal){
         this.meals.add(meal);
@@ -37,8 +100,8 @@ public class Meals {
     }
 
     public void createMealPlan(){
-        if (this.meals.size()<7){
-            throw new RejectedExecutionException("Add more meals before creating a meal plan!");
+        if (this.meals.size()<6){
+            throw new RejectedExecutionException("Add more meals before creating a meal plan!\n");
         }
         List<String> tempMeals = new ArrayList<>(this.meals);
 
@@ -55,19 +118,17 @@ public class Meals {
     }
 
     public void displayMealPlan(){
-        String[] weekdays = {"Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"};
 
         for (int i = 0; i < 7; i++) {
             System.out.println(weekdays[i] + this.mealPlan.get(i));
         }
     }
 
-
     public static void main(String[] args) {
-
-
+        JFrame frame = new JFrame("Meals");
+        frame.setContentPane(new Meals().JPanel);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
     }
-
-
-
 }
